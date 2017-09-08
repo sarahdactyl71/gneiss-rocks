ac37acfad8e6c405cb970bdee25276bbf792c3c5
Sarah Kirk <kirkse710@gmail.com>
2017-09-08 14:06:15 -0600

    changes the conn with a defaut of get to a post

--- app/services/google_vision_service.rb
+++ app/services/google_vision_service.rb
@@ -3,7 +3,8 @@ class GoogleVisionService
   attr_reader: conn
 
   def initialize
-    @conn = Faraday.new(url: "https://vision.googleapis.com/v1/images:annotate?key=#{ENV["vision_api_key"]}") do |req|
+    @conn = Faraday.new(url: "https://vision.googleapis.com/v1/images:annotate?key=#{ENV["vision_api_key"]}")
+    @conn.post do |req|
       req.body = '{
                     "requests": [
                       {
