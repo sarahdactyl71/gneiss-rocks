6314dc41f94a83311330dea3709291c3c89f800d
Sarah Kirk <kirkse710@gmail.com>
2017-09-07 18:46:12 -0600

    creates strong params for rock class

--- app/controllers/rocks_controller.rb
+++ app/controllers/rocks_controller.rb
@@ -1,7 +1,12 @@
 class RocksController < ApplicationController
 
   def index
-    # @uploader = ImageUploader.new
+
   end
 
+  private
+
+  def rock_params
+    params.require(:rock).permit(:location_found, :description, :image)
+  end
 end
