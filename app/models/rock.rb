c8ef3000874ba13ba74aa3b41108588d523a64f4
Sarah Kirk <kirkse710@gmail.com>
2017-09-09 13:52:23 -0600

    reset the database

--- app/models/rock.rb
+++ app/models/rock.rb
@@ -3,4 +3,27 @@ class Rock < ApplicationRecord
 
   validates_attachment :image,
                      content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+
+ # def detect_web project_id:, image_path:
+ #   # [START vision_web_detection]
+ #   # project_id = "gniess-rocks"
+ #   # image_path = "Path to local image file, eg. './image.png'"
+ #
+ #   require "google/cloud/vision"
+ #
+ #   vision = Google::Cloud::Vision.new project: project_id
+ #   image  = vision.image image_path
+ #
+ #   web = image.web
+ #
+ #   web.entities.each do |entity|
+ #     puts entity.description
+ #   end
+ #
+ #   web.full_matching_images.each do |image|
+ #     puts image.url
+ #   end
+ #   # [END vision_web_detection]
+ # end
+
 end
--- app/services/google_vision_service.rb
+++ app/services/google_vision_service.rb
@@ -23,6 +23,26 @@ class GoogleVisionService
       req.adapter  Faraday.default_adapter
   end
 
-
+  # def detect_web project_id:, image_path:
+  #   # [START vision_web_detection]
+  #   # project_id = "Your Google Cloud project ID"
+  #   # image_path = "Path to local image file, eg. './image.png'"
+  #
+  #   require "google/cloud/vision"
+  #
+  #   vision = Google::Cloud::Vision.new project: project_id
+  #   image  = vision.image image_path
+  #
+  #   web = image.web
+  #
+  #   web.entities.each do |entity|
+  #     puts entity.description
+  #   end
+  #
+  #   web.full_matching_images.each do |image|
+  #     puts image.url
+  #   end
+  #   # [END vision_web_detection]
+  # end
 
 end
