0600a30a82a166d5aaaf4ddaca11c2dc571b9c1c
Sarah Kirk <kirkse710@gmail.com>
2017-09-09 11:44:56 -0600

    comments out encoding to base64 in create action in frock controller

--- app/controllers/rocks_controller.rb
+++ app/controllers/rocks_controller.rb
@@ -14,8 +14,8 @@ class RocksController < ApplicationController
     @rock = Rock.create!(rock_params)
 
     if @rock.save
-      encoded_image = Base64.encode64(@rock.image)
-      Rock.update(@rock.id, image: encoded_image)
+      # encoded_image = Base64.encode64(@rock.image)
+      # Rock.update(@rock.id, image: encoded_image)
       flash[:success] = "Rock has been successfully created!"
       redirect_to root_path
     else
--- app/models/rock.rb
+++ app/models/rock.rb
@@ -1,4 +1,6 @@
 class Rock < ApplicationRecord
   has_attached_file :image
 
+  validates_attachment :image,
+                     content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
