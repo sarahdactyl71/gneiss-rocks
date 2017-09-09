ca2e85589f6dacb827bb3bdbd1b7c3ec423af64a
Sarah Kirk <kirkse710@gmail.com>
2017-09-09 11:09:43 -0600

    changes application.html.erb to have a simple uploader

--- app/controllers/rocks_controller.rb
+++ app/controllers/rocks_controller.rb
@@ -15,7 +15,7 @@ class RocksController < ApplicationController
     # @rock = Rock.create!(rock_params).encode_base_six_four(@rock.image)
 
     if @rock.save
-      encoded_image = Base64,encode64(@rock.image)
+      encoded_image = Base64.encode64(@rock.image)
       Rock.update(@rock.id, image: encoded_image)
       flash[:notice] = "Rock has been successfully created!"
       redirect_to root_path
