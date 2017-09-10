4e555120cba1ea2041aac678bded392a52b47680
Sarah Kirk <kirkse710@gmail.com>
2017-09-10 17:09:45 -0600

    starts writing test for rock label detection

---
+++ spec/features/user_can_see_label_detection_spec.rb
@@ -0,0 +1,15 @@
+require 'rails-helper'
+
+RSpec.describe "a user can navigate to rock show page" do
+  it "they can see labels about the rock image" do
+
+  end
+end
+
+# As a user
+# when I visit root path
+# and I click on "find out about this rock"
+# I am directed to the rocks show page
+# and a google label detection has been run
+# I expect the page to have 5 or more labels
+# i expect the page to have the word "Rock"
