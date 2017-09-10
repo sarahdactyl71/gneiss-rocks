3b07918ed44324ab95a7c83ee04b8a953a62337a
Sarah Kirk <kirkse710@gmail.com>
2017-09-10 15:29:06 -0600

    changes method name that detects labels to detect_labels, adds method called webdetection

--- app/models/rock.rb
+++ app/models/rock.rb
@@ -3,7 +3,7 @@ class Rock < ApplicationRecord
 
   validates_attachment :image,
                      content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
-  def web_detection
+  def detect_labels
     # Imports the Google Cloud client library
     require "google/cloud/vision"
 
@@ -22,6 +22,24 @@ class Rock < ApplicationRecord
     puts "Labels:"
     labels.each do |label|
      puts label.description
+    end
+  end
+
+  def web_detection
+    project_id = "gneiss-rocks"
+    image_path = "/Users/sarahkirk/turing/4module/gneiss-rocks/public/system/rocks/images/000/000/001/original/other_rock.JPG"
+
+    require "google/cloud/vision"
+
+    # Your Google Cloud Platform project ID
+
+    vision = Google::Cloud::Vision.new project: project_id
+    image  = vision.image image_path
+
+    web = image.web
+
+    web.entities.each do |entity|
+      puts entity.description
     end
   end
 end
