26c6fd407d8c2890151c6a940131b8bbe1658d7f
Sarah Kirk <kirkse710@gmail.com>
2017-09-11 14:13:47 -0600

    tries to make different image sizing with paperclip

--- app/models/rock.rb
+++ app/models/rock.rb
@@ -1,5 +1,9 @@
 class Rock < ApplicationRecord
-  has_attached_file :image
+  has_attached_file :image,
+                    :styles => {
+                    :thumb => "100x100#",
+                    :small  => "150x150>",
+                    :medium => "500x500" }
 
   validates_attachment :image,
                      content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
