0e3502fbc9171b949085c44df505b956d8ff48b7
Sarah Kirk <kirkse710@gmail.com>
2017-09-10 18:41:43 -0600

    updates tests to find input fields

--- spec/features/a_user_can_create_a_rock_spec.rb
+++ spec/features/a_user_can_create_a_rock_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "a user can create a rock" do
   it "sees all the attributes a rock has" do
     visit new_rock_path
-    fill_in rock_location_found, with: "Boulder, CO"
-    fill_in rock_description, with: "Conglomerate"
+    fill_in 'Location found', with: "Boulder, CO"
+    fill_in 'Description', with: "Conglomerate"
     attach_file("Choose File", Rails.root + "spec/fixtures/rock.jpg")
     click_on "Create Rock"
     expect(current_path).to eq(root_path)
