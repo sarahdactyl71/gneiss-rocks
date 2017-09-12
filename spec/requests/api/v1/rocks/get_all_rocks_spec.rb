8b3358ccaf801c8506edbe3901b675cb69b41275
Sarah Kirk <kirkse710@gmail.com>
2017-09-12 10:08:05 -0600

    writes test for showing a single rock

--- spec/requests/api/v1/rocks/get_all_rocks_spec.rb
+++ spec/requests/api/v1/rocks/get_all_rocks_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
-describe "get all the rocks" do
+describe "Rocks API" do
   it "returns all the rocks in the database" do
-    create(:rock, location_found: "Boulder")
-    create(:rock, location_found: "Denver")
-    create(:rock, location_found: "Santa Fe")
+    rocks = create_list(:rock, 3)
 
-    get "/api/v1/rocks/all"
+    get "/api/v1/rocks"
 
     JSON.parse(response.body)
+
+    expect(response).to be_success
+  end
+
+  it "shows a single rock" do
+    rocks = create_list(:rock, 3)
+    rock = rocks.first
+
+    get "/api/v1/items/#{rock.id}"
 
     expect(response).to be_success
   end
