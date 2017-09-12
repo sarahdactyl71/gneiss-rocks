c723e5082c991c8891896db42d606fcbf490a1ee
Sarah Kirk <kirkse710@gmail.com>
2017-09-12 13:14:16 -0600

    updates show method for rock api to be a little less redundant

--- app/controllers/api/v1/rocks_controller.rb
+++ app/controllers/api/v1/rocks_controller.rb
@@ -5,12 +5,28 @@ class Api::V1::RocksController < ApplicationController
   end
 
   def show
-    rock = Rock.find_by(id: params[:id])
+    rock = Rock.find(params[:id])
     if rock
       render json: rock
     else
       render json: {"Error": "Item not found"}, status: 404
     end
+  end
+
+  def create
+    rock = Rock.new(rock_params)
+
+    if rock.save
+      render json: rock
+    else
+      render json: rock.errors, status: 400
+    end
+  end
+
+  private
+
+  def rock_params
+    params.require(:rock).permit(:location_found, :description, :image, :image_file_name, :image_file_size, :image_content_type, :image_updated_at)
   end
 
 end
