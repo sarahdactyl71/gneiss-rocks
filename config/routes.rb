99b95bc0047db98846375b40d2605da68385b784
Sarah Kirk <kirkse710@gmail.com>
2017-09-12 10:58:59 -0600

    adds a namespace to the routes configureation, want to make a method of webdetection

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :rocks do
+        get '/:id/web_detection', to: 'web_detection#show'
+      end
       resources :rocks, only: [:index, :show]
       end
     end
