3667a8fe73ee2de36c2e22b261dd76ce623077cb
Sarah Kirk <kirkse710@gmail.com>
2017-09-12 11:03:26 -0600

    adds rock directory under api/v1

--- spec/requests/api/v1/rocks/rock_web_detection_spec.rb
+++ spec/requests/api/v1/rocks/rock_web_detection_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 describe "Rock Web Detection API" do
-  it "returns a list of scores and descriptions associated with web detection"
-    visit new_rock_path
-    fill_in 'Location found', with: "Boulder, CO"
-    fill_in 'Description', with: "Conglomerate"
-    attach_file("Image", Rails.root + "spec/fixtures/rock.jpg")
-    click_on "Create Rock"
+  it "returns a list of scores and descriptions associated with web detection" do
+    # visit new_rock_path
+    # fill_in 'Location found', with: "Boulder, CO"
+    # fill_in 'Description', with: "Conglomerate"
+    # attach_file("Image", Rails.root + "spec/fixtures/rock.jpg")
+    # click_on "Create Rock"
+    rock = create(:rock)
 
     get "/api/v1/rocks/#{rock.id}/web_detection"
 
