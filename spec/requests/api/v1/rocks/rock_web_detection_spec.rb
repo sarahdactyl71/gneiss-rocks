ac349ed8bb61c7b9286b9928e38e0a24bf7276f4
Sarah Kirk <kirkse710@gmail.com>
2017-09-12 10:53:05 -0600

    writes test for API webdetection

--- spec/requests/api/v1/rocks/rock_web_detection_spec.rb
+++ spec/requests/api/v1/rocks/rock_web_detection_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 describe "Rock Web Detection API" do
   it "returns a list of scores and descriptions associated with web detection"
-    
+    visit new_rock_path
+    fill_in 'Location found', with: "Boulder, CO"
+    fill_in 'Description', with: "Conglomerate"
+    attach_file("Image", Rails.root + "spec/fixtures/rock.jpg")
+    click_on "Create Rock"
+
+    get "/api/v1/rocks/#{rock.id}/web_detection"
+
+    expect(response).to be_success
   end
 end
