59625e8439bf12c29c5266669215ef3b6446486c
Sarah Kirk <kirkse710@gmail.com>
2017-09-14 11:09:38 -0600

    makes sure development.rb has the proper environment configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,14 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV["bucket_name"],
+      access_key_id: ENV["access_key"],
+      secret_access_key: ENV["secret_key"],
+      s3_region: ENV["region"],
+    }
+  }
 end
