ae450d0fcfef2b6749e062951bcdb9e7b41c4303
Sarah Kirk <kirkse710@gmail.com>
2017-09-14 09:31:28 -0600

    writes default configurations to production.rb, installs gem AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('gneiss-rocks'),
+      access_key_id: ENV.fetch('AKIAJWNXNE5HQUNFRTCA'),
+      secret_access_key: ENV.fetch('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'),
+      s3_region: ENV.fetch('us-east-2'),
+    }
+  }
 end
