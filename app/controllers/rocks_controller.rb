2a00624d48bbd3baf97c3332d46734390ac4dfc7
Sarah Kirk <kirkse710@gmail.com>
2017-09-16 15:17:31 -0600

    changes things with credentials

--- app/controllers/rocks_controller.rb
+++ app/controllers/rocks_controller.rb
@@ -23,7 +23,6 @@ class RocksController < ApplicationController
 
   def show
     @rock = Rock.find(params[:id])
-    # image_path = @rock.image.path
     image_path = @rock.public_url
     @web_info = @rock.web_detection(image_path)
     @label_info = @rock.detect_labels(image_path)
