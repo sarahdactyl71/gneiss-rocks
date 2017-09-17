d661d42778d2d5e2246015d9c0824606e317e498
Sarah Kirk <kirkse710@gmail.com>
2017-09-17 09:50:01 -0600

    starts adding a jquery method for making html for each rock, updates the rock url in the normal html

--- app/assets/javascripts/rocks.js
+++ app/assets/javascripts/rocks.js
@@ -8,6 +8,19 @@ function Rock (rock) {
   this.public_url = rock.public_url
 }
 
+Rock.prototype.toHML= function () {
+  return `<div class="media">
+            <div class="media-left">
+              <%= link_to image_tag(rock.image.url, class: 'media-object'), rock.image.url, target: '_blank' %>
+            </div>
+            <div class="media-body">
+              <h4 class="media-heading"><%= rock.location_found %></h4>
+              <h4 class="media-heading"><%= rock.description %></h4>
+              <%= link_to("What Is This Rock?", rock_path(rock.id), class: "btn") %>
+            </div>
+          </div>`
+}
+
 Rock.getAllRocks = function () {
   return $.getJSON(`${API}/api/v1/rocks`)
 }
