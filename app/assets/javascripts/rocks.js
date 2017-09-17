a7fa779fa7ad32183f866c9cc6202c6089466ea4
Sarah Kirk <kirkse710@gmail.com>
2017-09-17 09:57:52 -0600

    adds a rock prototype to html function in JS file

--- app/assets/javascripts/rocks.js
+++ app/assets/javascripts/rocks.js
@@ -11,12 +11,12 @@ function Rock (rock) {
 Rock.prototype.toHML= function () {
   return `<div class="media">
             <div class="media-left">
-              <%= link_to image_tag(rock.image.url, class: 'media-object'), rock.image.url, target: '_blank' %>
+              <%= link_to image_tag(${this.public_url}, class: 'media-object'), ${this.public_url}, target: '_blank' %>
             </div>
             <div class="media-body">
-              <h4 class="media-heading"><%= rock.location_found %></h4>
-              <h4 class="media-heading"><%= rock.description %></h4>
-              <%= link_to("What Is This Rock?", rock_path(rock.id), class: "btn") %>
+              <h4 class="media-heading"><%= ${this.location_found} %></h4>
+              <h4 class="media-heading"><%= ${this.description} %></h4>
+              <%= link_to("What Is This Rock?", rock_path(${this.id}), class: "btn") %>
             </div>
           </div>`
 }
