e8cbba5bfd3dc200293299889ddbcd17aece8541
Sarah Kirk <kirkse710@gmail.com>
2017-09-29 15:14:47 -0600

    deletes comments from test

--- spec/features/a_user_can_create_a_rock_spec.rb
+++ spec/features/a_user_can_create_a_rock_spec.rb
@@ -13,16 +13,3 @@ RSpec.describe "a user can create a rock" do
     expect(page).to have_selector("img")
   end
 end
-
-# as a user
-# when I visit the home page
-# and I click on "create new rock"
-# I expect my current path to be the new rock path
-# I fill in location found with something
-# I fill in description with something
-# and I upload an image
-# I click on create rock
-# I expect my current path to be the rock index
-# and I expect the page to have the location I submitted,
-# the description I submitted,
-# and the image I submitted
